a3a403cb75e963ee2ea7411e6cc6fd0701f5e16c
Jari Aalto <jari.aalto@cante.net>
2012-01-15 23:36:58 +0200

    epackage/: *-epackage-*
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/lib-read-char-spec-epackage-autoloads.el
+++ epackage/lib-read-char-spec-epackage-autoloads.el
@@ -1,4 +1,4 @@
 (autoload 'read-char-spec                       "read-char-spec" "" nil)
 (autoload 'read-char-spec-format-key            "read-char-spec" "" nil)
 (autoload 'read-char-spec-generate-help         "read-char-spec" "" nil)
-(provide 'lib-read-char-spec-epkg-autoloads)
+(provide 'lib-read-char-spec-epackage-autoloads)
